81e34c66eaf73bfc184241d726b8d7445816768e
AyushGameDev <ayushtechportal@gmail.com>
2020-04-09 17:28:46 +0530

    Short names added

--- deccan.h
+++ deccan.h
@@ -10,3 +10,36 @@
 
 #include "src/utils/log.h"
 #include "src/utils/timer.h"
+
+#ifdef using_namespace_deccan
+    #define DECCAN_SHORT_NAMES
+#endif
+
+#ifdef DECCAN_SHORT_NAMES 
+    #define Info deccan_Info
+    #define Scene deccan_Scene
+    #define KeyState deccan_KeyState
+    #define MouseState deccan_MouseState
+    #define MousePos deccan_MousePos
+    #define Color deccan_Color
+    #define Timer deccan_Timer 
+
+    #define set_global_engine deccan_set_global_engine
+    #define get_global_engine deccan_get_global_engine
+    #define init deccan_init
+    #define quit deccan_quit
+    #define run deccan_run
+    #define new_scene deccan_new_scene
+    #define add_scene deccan_add_scene
+    #define input_get_key deccan_input_get_key
+    #define input_get_mouse_button deccan_input_get_mouse_button
+    #define input_get_mouse_pos deccan_input_get_mouse_pos
+    #define renderer_clear deccan_renderer_clear
+    #define renderer_set_color deccan_renderer_set_color
+    #define timer_init deccan_timer_init
+    #define timer_start deccan_timer_start
+    #define timer_stop deccan_timer_stop
+    #define timer_pause deccan_timer_pause
+    #define timer_get_time deccan_timer_get_time
+    #define timer_get_time_ms deccan_timer_get_time_ms
+#endif
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#define using_namespace_deccan
 #include "deccan.h"
 
 void begin() {
@@ -7,26 +8,26 @@ void begin() {
 int x = 100, y = 100;
 
 void step() {    
-    deccan_renderer_clear((deccan_Color){255, 255, 255, 255});
+    renderer_clear((Color){255, 255, 255, 255});
 
     /* Start here */
 #ifdef DECCAN_DEBUG
-    if(deccan_input_get_key(SDL_SCANCODE_UP).is_held){ y-=1; }
-    else if(deccan_input_get_key(SDL_SCANCODE_DOWN).is_held){ y+=1; }
-    else if(deccan_input_get_key(SDL_SCANCODE_LEFT).is_held){ x-=1; }
-    else if(deccan_input_get_key(SDL_SCANCODE_RIGHT).is_held){ x+=1; }
+    if(input_get_key(SDL_SCANCODE_UP).is_held){ y-=1; }
+    else if(input_get_key(SDL_SCANCODE_DOWN).is_held){ y+=1; }
+    else if(input_get_key(SDL_SCANCODE_LEFT).is_held){ x-=1; }
+    else if(input_get_key(SDL_SCANCODE_RIGHT).is_held){ x+=1; }
     
-    deccan_MousePos pos = deccan_input_get_mouse_pos();
+    MousePos pos = input_get_mouse_pos();
     if(pos.x > x && pos.x < x+50 && pos.y > y && pos.y < y+50) {
-        if(deccan_input_get_mouse_button(SDL_BUTTON_LEFT).is_down) {
-            deccan_renderer_set_color((deccan_Color){0, 255, 0, 255});
+        if(input_get_mouse_button(SDL_BUTTON_LEFT).is_down) {
+            renderer_set_color((Color){0, 255, 0, 255});
         }
-        else { deccan_renderer_set_color((deccan_Color){0, 180, 0, 255}); }
+        else { renderer_set_color((Color){0, 180, 0, 255}); }
     }
     else {
-        deccan_renderer_set_color(black);
+        renderer_set_color(black);
     }
-    SDL_RenderFillRect(deccan_get_global_engine()->renderer, &(SDL_Rect){x, y, 50, 50});
+    SDL_RenderFillRect(get_global_engine()->renderer, &(SDL_Rect){x, y, 50, 50});
 #endif
 }
 
@@ -35,9 +36,9 @@ void end() {
 }
 
 int main(int argc, char **argv) {
-    if(deccan_init("Test", 640, 320)) {
-        deccan_add_scene(deccan_new_scene("scene0", begin, step, end));
-        deccan_run(120.0f);
+    if(init("Test", 640, 320)) {
+        add_scene(new_scene("scene0", begin, step, end));
+        run(120.0f);
     }
-    atexit(deccan_quit);
+    atexit(quit);
 }
