9364a1b6646f1f9dfcb61a799fda0933f6ab38b1
iddev5 <ayushtechportal@gmail.com>
2020-09-02 23:20:02 +0530

    Added compiler macros
    Compiled macros for detecting:
    
    * C standard
    * C compiler
    * OS-family
    * OS

--- corelib/Config.h
+++ corelib/Config.h
@@ -23,11 +23,76 @@
 #include "../depends/stb/stb_image.h"
 #include "../depends/stb/stb_ds.h"
 
-/* Define basic constants */
-#ifndef PI
-    #define PI 3.14159265358979323846
+/////////////////////////////////////////////////
+/* Detect C Version */
+/////////////////////////////////////////////////
+#if __STDC_VERSION__ < 201112L
+    #error Use a modern C compiler(C11 or greater)
 #endif
 
+/////////////////////////////////////////////////
+/* Detect C Compiler */
+/////////////////////////////////////////////////
+#if defined(__clang__)
+    #define DE_COMPILER_CLANG
+#elif defined(_MSC_VER_)
+    #define DE_COMPILER_MSVC
+#elif defined(__GNUC__)
+    #define DE_COMPILER_GCC
+#elif defined(__MINGW32__)
+    #define DE_COMPILER_MINGW
+#else
+    // TODO: Pelles C and others?
+    #error This C compiler is not supported
+#endif
+
+/////////////////////////////////////////////////
+/* Detect OS-family */
+/////////////////////////////////////////////////
+#if defined(unix) || defined(__unix__) || defined(__unix)
+    #define DE_PLATFORM_UNIX
+#endif
+
+/////////////////////////////////////////////////
+/* Detect OS */
+/////////////////////////////////////////////////
+#if defined(__ANDROID__)
+    #define DE_OS_ANDROID
+#elif defined(__linux__)
+    #define DE_OS_LINUX
+#elif defined(__FreeBSD__) && defined(__NetBSD__) && \
+    defined(__OpenBSD__) && defined(__DragonFly__)
+    #define DE_OS_BSD
+#elif defined(__APPLE__) && defined(__MACH__)
+    #include <TargetConditionals.h>
+    #if defined(TARGET_OS_IPHONE) || defined(TARGET_IPHONE_SIMULATOR)
+        #define DE_OS_IOS
+    #elif TARGET_OS_MAC
+        #define DE_OS_MAC
+    #else
+        #error "This Apple operating system is not supported"
+    #endif
+#elif defined(_WIN32)
+    #if defined(DE_COMPILER_MSVC) && defined(_WINRT_DLL)
+        #define DE_OS_UWP
+    #else
+        #define DE_OS_WIN
+    #endif
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
+#elif defined(__EMSCRIPTEN__)
+    #define DE_OS_WEB
+#else
+    #error This Operating System is not supported
+#endif
+
+
+/////////////////////////////////////////////////
+/* Define basic constants */
+/////////////////////////////////////////////////
+
+#define PI 3.14159265358979323846
 #define DEG2RAD (PI/180.0000)
 #define RAD2DEG (180.0000/PI)
 
