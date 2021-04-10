1fd02d7e25b5ecd6e2f72c875781aed2e97de4ab
iddev5 <ayushtechportal@gmail.com>
2021-04-10 18:03:18 +0530

    Added custom handle pool implementation

---
+++ corelib/Core/Handle.c
@@ -0,0 +1,67 @@
+#include "Handle.h"
+
+/////////////////////////////////////////////////
+// Handle Pool
+////////////////////////////////////////////////
+
+DeccanHandlePool *DE_HandlePoolCreate(int capacity) {
+    if (capacity <= 0) 
+        return;
+
+    DeccanHandlePool *pool = DE_Alloc(sizeof(DeccanHandlePool), 1);
+    pool->dense  = DE_Alloc(sizeof(DeccanHandle), capacity);
+    pool->sparse = DE_Alloc(sizeof(uint32_t), capacity);
+    pool->length = 0;
+    for (int i = 0; i < pool->capacity; i += 1) {
+        pool->dense[i] = i;
+    }
+}
+
+void DE_HandlePoolDestroy(DeccanHandlePool *pool) {
+    DE_Free(pool->sparse);
+    DE_Free(pool->dense);
+    DE_Free(pool);
+}
+
+/////////////////////////////////////////////////
+// Handle
+////////////////////////////////////////////////
+
+DeccanHandle DE_HandleNew(DeccanHandlePool *pool) {
+    if (pool->length < pool->capacity) {
+        int index = length;
+        length += 1;
+
+        DeccanHandle handle = pool->dense[index];
+        pool->sparse[handle] = index;
+        return handle;
+    }
+    else {
+        pool->capacity *= 2;
+        pool->dense = DE_Realloc(pool->dense, pool->capacity);
+        pool->sparse = DE_Realloc(pool->sparse, pool->capacity);
+        return DE_HandleNew(pool);
+    }
+
+    return -1;
+}
+
+void DE_HandleDelete(DeccanHandlePool *pool, DeccanHandle handle) {
+    uint32_t index = pool->sparse[handle];
+    pool->length -= 1;
+
+    DeccanHandle last_handle = pool->dense[pool->length];
+
+    pool->dense[pool->length] = handle;
+    pool->sparse[last_handle] = index;
+    pool->dense[index] = last_handle;
+}
+
+uint32_t DE_HandleIndex(DeccanHandlePool *pool, DeccanHandle handle) {
+    return pool->sparse[handle];
+}
+
+bool DE_HandleValid(DeccanHandlePool *pool, DeccanHandle handle) {
+    uint32_t index = pool->sparse[handle];
+    return index < pool->length && pool->dense[index] == handle; 
+}
---
+++ corelib/Core/Handle.h
@@ -0,0 +1,38 @@
+/* Deccan Game Engine - C11 2D SDL Game Engine.
+ * Copyright 2020, 2021 Ayush Bardhan Tripathy
+ *
+ * This software is licensed under MIT License.
+ * See LICENSE.md included with this package for more info.
+ */
+
+#pragma once
+#include "../Config.h"
+
+/////////////////////////////////////////////////
+// Structs
+////////////////////////////////////////////////
+
+typedef uint32_t DeccanHandle;
+
+typedef struct DeccanHandlePool {
+    DeccanHandle *dense;
+    uint32_t *sparse;
+    int length;
+    int capacity;
+} DeccanHandlePool;
+
+/////////////////////////////////////////////////
+// Handle Pool
+////////////////////////////////////////////////
+
+DeccanHandlePool *DE_HandlePoolCreate(int capacity);
+void DE_HandlePoolDestroy(DeccanHandlePool *pool);
+
+/////////////////////////////////////////////////
+// Handle
+////////////////////////////////////////////////
+
+DeccanHandle DE_HandleNew(DeccanHandlePool *pool);
+void DE_HandleDelete(DeccanHandlePool *pool, DeccanHandle handle);
+uint32_t DE_HandleIndex(DeccanHandlePool *pool, DeccanHandle handle);
+bool DE_HandleValid(DeccanHandlePool *pool, DeccanHandle handle);
